2905e82435c79e6fac5955024cb0583da416a427
Danielle Sophia <lr.dspasion@mmdc.mcl.edu.ph>
2026-03-07 20:33:33 +0800

    Add files via upload

---
+++ (FINAL) IPTGroup2-MS2-MH7-Codes/IPTGroup2-MS2-HM7-Codes/connectly_project/posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.author.username}"
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post') 
+
+    def __str__(self):
+        return f"{self.user.username} liked Post {self.post.id}"
---
+++ (FINAL) IPTGroup2-MS2-MH7-Codes/IPTGroup2-MS2-HM7-Codes/connectly_project/posts/urls.py
@@ -0,0 +1,50 @@
+from django.urls import path
+from . import views
+from .views import PostListCreate, CommentListCreate, PostDetail,  CommentDetail
+from .views import ProtectedView
+from .views import PostLikeView, PostCommentView, PostCommentListView
+from .views import GoogleLoginView
+from .views import GoogleCallbackView
+
+
+urlpatterns = [
+    
+    #USERS
+    path('users/', views.list_users, name='list-users'),
+    path('users/create/', views.create_user, name='create-user'),
+    path('users/<int:user_id>/update/', views.update_user, name='update-user'),
+    path('users/<int:user_id>/delete/', views.delete_user, name='delete-user'),
+ 
+
+    
+    #LOGIN
+    path('login/', views.login_user, name='login'),
+    
+    #Posts
+    path('posts/', PostListCreate.as_view(), name='post-list-create'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    
+    #comments
+    path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
+
+    
+    #protected
+    path('protected/', ProtectedView.as_view(), name='protected'),
+    
+    
+    # like
+    path('posts/<int:pk>/like/', PostLikeView.as_view(), name='post-like'),
+
+# comment under post
+    path('posts/<int:pk>/comment/', PostCommentView.as_view(), name='post-comment'),
+
+# get comments per post
+    path('posts/<int:pk>/comments/', PostCommentListView.as_view(), name='post-comments'),  
+    
+# google oauth
+path('auth/google/login/', GoogleLoginView.as_view(), name='google-login'),
+
+path('auth/google/callback/', GoogleCallbackView.as_view(), name='google-callback'),
+
+]
